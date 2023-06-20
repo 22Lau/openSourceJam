0951b646306fc0909040ad81bd448ae43c99db82
LindaHG1 <116809226+LindaHG1@users.noreply.github.com>
2023-06-20 13:57:48 +0200

    transition added

--- main.js
+++ main.js
@@ -10,3 +10,16 @@ navToggle.addEventListener("click", () => {
         navToggle.setAttribute("aria-label", "Abrir menú");
     }
 });
+
+$(document).ready(function () {
+    $('a[href^="#"]').on('click', function (event) {
+        var target = $(this.getAttribute('href'));
+
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1500); 
+        }
+    });
+});
