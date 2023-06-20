9e48317746cb31f217fa0677502ad2fac3c9af68
LindaHG1 <116809226+LindaHG1@users.noreply.github.com>
2023-06-20 22:55:05 +0200

    changes

--- main.js
+++ main.js
@@ -11,15 +11,86 @@ navToggle.addEventListener("click", () => {
     }
 });
 
-$(document).ready(function () {
-    $('a[href^="#"]').on('click', function (event) {
-        var target = $(this.getAttribute('href'));
+/* ----TIMELINE----- */
 
-        if (target.length) {
-            event.preventDefault();
-            $('html, body').stop().animate({
-                scrollTop: target.offset().top
-            }, 1500); 
+(function () {
+    "use strict";
+
+    // define variables
+    var items = document.querySelectorAll(".timeline li");
+
+    function isElementInViewport(el) {
+        var rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <=
+            (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    function callbackFunc() {
+        for (var i = 0; i < items.length; i++) {
+            if (isElementInViewport(items[i])) {
+            items[i].classList.add("in-view");
+            }
         }
-    });
+        }
+
+    // listen for events
+    window.addEventListener("load", callbackFunc);
+    window.addEventListener("resize", callbackFunc);
+    window.addEventListener("scroll", callbackFunc);
+    })();
+
+   /*  --CONTACT FORM--- */
+
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+            e.preventDefault();
+
+            var target = this.hash;
+            var $target = $(target);
+
+            $('html, body').stop().animate({
+                'scrollTop': $target.offset().top
+            }, 900, 'swing', function () {
+                window.location.hash = target;
+            });
+        });
 });
+
+function submitForm() {
+
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    
+    const successMessage = document.getElementById("success-message");
+    successMessage.style.display = "block";
+    successMessage.innerHTML = "¡Tu mensaje ha sido enviado exitosamente!";
+
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+
+    
+    setTimeout(function() {
+        successMessage.style.display = "none";
+    }, 5000);
+}
+
+
+window.addEventListener('load', () => {
+    anime({
+        targets: '.container',
+        translateX: ['100%', '0'],
+        opacity: [0, 1],
+        duration: 1000,
+        easing: 'easeOutExpo'
+        });
+    });
